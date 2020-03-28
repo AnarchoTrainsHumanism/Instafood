3b1ed0cc20b09ed5d38757335357eb9b3be439b5
Sunil <srao0194@protonmail.com>
2020-03-28 13:04:48 -0500

    updated sandbox scene

---
+++ Instafood/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class Dialogue : MonoBehaviour
+{
+    public TextAsset textAsset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // TODO: Read through text asset and set up dialogue system
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
